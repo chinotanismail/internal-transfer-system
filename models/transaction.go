446244dbf797b1783740f2f8acd702b6e4fd046e
Chino Tan Ismail <chino.ismail@gmail.com>
2025-06-15 19:59:43 +0700

    Fix response format

--- models/transaction.go
+++ models/transaction.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type Transaction struct {
-	ID                   uint `gorm:"primaryKey"`
-	SourceAccountID      uint
-	DestinationAccountID uint
-	Amount               float64
+	ID                   uint      `gorm:"primaryKey"`
+	SourceAccountID      uint      `json:"source_account_id"`
+	DestinationAccountID uint      `json:"destination_account_id"`
+	Amount               float64   `json:"amount"`
 	CreatedAt            time.Time `json:"created_at"`
 	Status               string    `json:"status"`
 	ErrorReason          string    `json:"error_reason,omitempty"`
